5d3db004ff3e35bb43bdd9bc87b339dd5ab9001d
G33k <delarueguillaume@gmail.com>
2014-05-22 22:21:21 +0200

    Improve across input

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 module.exports = function(libs) {
-    if (typeof libs == 'object') libs = [libs];
+
+    // Arrayize 'libs'
+    if (typeof libs == 'string') libs = [libs];
+    else if (typeof libs == 'object') libs = [libs];
+
+    // Walk over libs and load them as global
     libs.forEach(function(mod, idx) {
+
+        // Lib will be named as his filename
         if (typeof mod == 'string') {
             GLOBAL[mod] = require(mod);
-        } else if (typeof mod == 'object') {
+        }
+
+        // Contains 'key' : 'lib'
+        else if (typeof mod == 'object') {
             for (var _name in mod) {
                 GLOBAL[_name] = require(mod[_name]);
             }
         }
+
     });
+
 }
