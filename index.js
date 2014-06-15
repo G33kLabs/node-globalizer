fb0fd07057768af7167480734d10bf264e568abf
G33k <delarueguillaume@gmail.com>
2014-06-16 00:03:22 +0200

    Not load libs that already have been loaded

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-module.exports = function(libs) {
+"use strict";
+module.exports = function (libs) {
 
     // Protect libs
-    if (!libs || typeof libs == 'undefined') return;
+    if(!libs || typeof libs === 'undefined') return;
 
     // Arrayize 'libs'
-    if (typeof libs == 'string') libs = [libs];
-    else if (typeof libs == 'object' && typeof libs.length != 'number') libs = [libs];
+    if(typeof libs === 'string') libs = [libs];
+    else if(typeof libs === 'object' && typeof libs.length !== 'number') libs = [libs];
 
     // Walk over libs and load them as global
-    libs.forEach(function(mod, idx) {
+    libs.forEach(function (mod) {
 
         // Lib will be named as his filename
-        if (typeof mod == 'string') {
+        if(typeof mod === 'string') {
             GLOBAL[mod] = require(mod);
         }
 
         // Contains 'key' : 'lib'
-        else if (typeof mod == 'object') {
-            for (var _name in mod) {
-                GLOBAL[_name] = require(mod[_name]);
+        else if(typeof mod === 'object') {
+            for(var _name in mod) {
+                GLOBAL[_name] = (typeof mod[_name] === 'string') ? require(mod[_name]) : mod[_name];
             }
         }
 
     });
 
-}
+};
